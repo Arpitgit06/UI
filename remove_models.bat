d26648915f0d87e2b40c679342de4774c5b6e997
Arpitgit06 <arpitsharma45039@gmail.com>
2026-08-06 22:58:47 +0530

    Models removal script update

--- remove_models.bat
+++ remove_models.bat
@@ -34,8 +34,8 @@ if exist "%PROJECT_ROOT%models_cache" (
     echo Deleting models_cache...
     rmdir /s /q "%PROJECT_ROOT%models_cache" 2>nul
 )
-if exist "%PROJECT_ROOT%yolov10n.pt" (
-    del /f /q "%PROJECT_ROOT%yolov10n.pt" 2>nul
+if exist "%PROJECT_ROOT%yolov*.pt" (
+    del /f /q "%PROJECT_ROOT%yolov*.pt" 2>nul
 )
 
 echo.
